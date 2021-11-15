5995274d667f63e22602f363731a56f6fc4900f3
Bishwal <maitri.bishwal@capgemini.com>
2021-11-15 14:54:36 +0530

    second commit

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+project_id="ferrous-biplane-329406"
+credential="C:\Users\mbishwal\ Downloads\ ferrous-biplane-329406-b1f44f0b40f3.json"
+bucket_name="bucket_1"
+storage_class="REGIONAL"
---
+++ variables.tf
@@ -0,0 +1,17 @@
+#provider
+variable "bucket_name"{
+
+}
+variable "bucket_location"{
+default="us-east1"
+
+}
+variable "project_id"{
+    
+}
+variable "storage_class"{
+
+}
+variable "credential"{
+
+}
